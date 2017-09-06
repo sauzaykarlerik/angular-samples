15355c55dfa5a4461ede89a3fc0a4135933460ac
Sauzay Karl erik <sauzaykarlerik@gmail.com>
2017-09-06 19:47:31 +0200

    Specific components & classes

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+// Import all components here
+import { HeroDetailComponent } from './hero-detail.component';
+
 @NgModule({
+  // Declare all components here
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
